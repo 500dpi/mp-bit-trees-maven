dadc825b65fd0448fac147ea6d3f0d13635ea96a
Sara_Jaljaa <jaljaasa@grinnell.edu>
2024-12-01 20:56:49 -0600

    + comments, formatting

--- src/main/java/edu/grinnell/csc207/main/BrailleASCII.java
+++ src/main/java/edu/grinnell/csc207/main/BrailleASCII.java
@@ -3,20 +3,27 @@ package edu.grinnell.csc207.main;
 import java.io.PrintWriter;
 
 /**
+ * Prints conversions between braille, ASCII, and Unicode.
  *
+ * @author Sara Jaljaa
  */
 public class BrailleASCII {
+
   // +------+--------------------------------------------------------
   // | Main |
   // +------+
 
   /**
+   * Prints the braille conversion.
    *
+   * @param args
+   *    The command line arguments, of the form:
+   *    [0] = <target conversion>
+   *    [1] = <source>
    */
   public static void main(String[] args) {
     PrintWriter pen = new PrintWriter(System.out, true);
     // STUB
     pen.close();
   } // main(String[]
-
 } // class BrailleASCII
